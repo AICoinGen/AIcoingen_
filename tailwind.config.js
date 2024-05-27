487e139af2643fc32a0c142160a4cfcadd1aaa14
AICoinGen <gonormonor@gmail.com>
2024-05-27 16:16:05 +0000

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   important: true,
-
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    join(__dirname, "src/**/*.{js,jsx,ts,tsx,vue,html,css}"),
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
