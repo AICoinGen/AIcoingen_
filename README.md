b4d073e8ad7a995dac87d9ce2d2afce7f2c6c953
AICoinGen <gonormonor@gmail.com>
2024-05-27 11:47:09 +0000

    update 5 files

---
+++ README.md
@@ -0,0 +1,145 @@
+# AICoinGen
+
+<p align="center">
+    <a href="https://www.youtube.com/watch?v=OpL5Q7Zc7qk" title="Project Demo">🖥️ Video</a>
+    .
+    <a href="https://github.com/mendsalbert/AICOINGEN" title="GitHub Repository">📂 Repo</a>
+    ·
+    <a href="https://github.com/mendsalbert/AICOINGEN/issues" title="Report Bug/Request Feature">🚀 Got Issue</a>
+</p>
+<a href="" title="Project Screenshot">
+    <img src="./styles/assets/img/front.png" width="100%" alt="Project Screenshot"/>
+</a>
+
+## Inspiration 💡
+
+In today's rapidly evolving digital world, there's a growing need for innovative tools that leverage artificial intelligence to simplify complex tasks. AICOINGEN was inspired by the potential of combining AI and decentralized finance (DeFi) to create a seamless, user-friendly platform for generating and managing ERC20 tokens. The goal was to empower users with an AI-driven solution that can suggest unique coin names and facilitate the creation of ERC20 tokens effortlessly.
+
+## What it Does ⚙️
+
+AICOINGEN is an AI-powered DeFi application designed to revolutionize how users create and manage ERC20 tokens. Here are some of its standout features:
+
+- **AI-Driven Coin Name Generation:** Uses advanced AI algorithms to suggest unique and catchy coin names.
+- **ERC20 Token Creation:** A user-friendly interface for creating ERC20 tokens on the Polygon Cardona Testnet.
+- **AICG Token:** The platform's native cryptocurrency, which powers various functionalities within the app.
+- **Chainlink Price Feed:** Integrates Chainlink Price Feeds to provide real-time price data for transactions.
+- **Chainlink Functions:** Utilizes Chainlink Functions for off-chain AI computations and bringing the results back on-chain.
+- **Donation System:** Users can donate MATIC to support the platform.
+- **Real-Time Dashboard:** Provides a comprehensive overview of created tokens, donations, and other key metrics.
+
+## How We Built It 🛠️
+
+AICOINGEN is built using a combination of cutting-edge technologies:
+
+- **Blockchain Backend:** Developed using Solidity, smart contracts on the Polygon Cardona Testnet provide a secure and decentralized backend for token management.
+- **Chainlink VRF & Price Feeds:** Used for generating unique identifiers and real-time price data.
+- **Chainlink Functions:** Powers the AI-driven coin name generation by performing off-chain computations.
+- **IPFS:** Ensures decentralized and secure storage of user data.
+- **React and Next.js:** Powers the dynamic and responsive front end.
+- **Tailwind CSS:** Facilitates rapid UI development with its utility-first framework.
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+- [Node.js](https://nodejs.org/en/)
+- [Git](https://git-scm.com/)
+- [npm](https://www.npmjs.com/)
+- [Hardhat](https://hardhat.org/)
+- Metamask (or any other Ethereum wallet)
+- Chainlink VRF setup (with LINK tokens)
+- IPFS setup
+
+### Installation Steps
+
+1. **Clone the repository:**
+
+```bash
+git clone https://github.com/mendsalbert/AICOINGEN.git
+cd aicoingen
+```
+
+2. **Install Dependencies:**
+
+```bash
+npm install
+```
+
+3. **Compile the Contracts:**
+
+```bash
+npx hardhat compile
+```
+
+4. **Deploy the Contracts:**
+
+```bash
+npx hardhat run scripts/deploy.js --network zkEVMCardonaTestnet
+```
+
+5. **Run the Development Server:**
+
+```bash
+npm start
+```
+
+6. **Open the App:**
+
+Navigate to `http://localhost:3000` in your web browser.
+
+**🎇 You are Ready to Go!**
+
+### Configuration
+
+To deploy to a test or main network, update the configurations located in `hardhat.config.js` to use a private key and deploy to the Polygon zkEVM Cardona Testnet.
+
+```javascript
+require("@nomiclabs/hardhat-waffle");
+const fs = require("fs");
+const { ethers } = require("ethers");
+const wallet = ethers.Wallet.fromMnemonic(seedPhrase);
+const privateKey = wallet.privateKey;
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    zkEVMCardonaTestnet: {
+      url: "https://polygon-zkevm-cardona.blockpi.network/v1/rpc/public",
+      accounts: [privateKey],
+    },
+  },
+  solidity: "0.8.24",
+  allowUnlimitedContractSize: true,
+  throwOnTransactionFailures: true,
+  throwOnCallFailures: true,
+};
+```
+
+## Smart Contract Overview
+
+The main contract, `LookUpContract`, manages ERC20 tokens and donations.
+
+### Key Functions
+
+- **createERC20Token:** Creates a new ERC20 token with specified details.
+- **getAllERC20TokenListed:** Retrieves all listed ERC20 tokens.
+- **getERC20Token:** Retrieves details of a specific ERC20 token by ID.
+- **getUserERC20Tokens:** Retrieves all ERC20 tokens owned by a specific user.
+- **donate:** Allows users to donate MATIC to the platform.
+- **getAllDonation:** Retrieves all donations made to the platform.
+- **sendRequest:** Sends a request to OpenAI via Chainlink Functions for coin name suggestions.
+- **fulfillRequest:** Handles the response from OpenAI and processes it.
+
+## Contributing
+
+We welcome contributions to AICOINGEN! To contribute, please follow these steps:
+
+1. Fork the repository.
+2. Create a new branch for your feature or bug fix.
+3. Make your changes and commit them.
+4. Push your changes to your fork.
+5. Submit a pull request to the main repository.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
--- contracts/LookUpContract.sol
+++ contracts/LookUpContract.sol
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
-contract LookUpContract {
-    
+import {FunctionsClient} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/FunctionsClient.sol";
+import {ConfirmedOwner} from "@chainlink/contracts/src/v0.8/shared/access/ConfirmedOwner.sol";
+import {FunctionsRequest} from "@chainlink/contracts/src/v0.8/functions/v1_0_0/libraries/FunctionsRequest.sol";
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+contract LookUpContract is FunctionsClient, ConfirmedOwner {
+    using FunctionsRequest for FunctionsRequest.Request;
+
     struct ERC20Token {
         uint256 tokenID;
         address owner;
@@ -19,19 +25,45 @@ contract LookUpContract {
         address donor;
         uint256 fund;
     }
-    
 
     address payable contractOwner = payable(0xb309098bcB51E5C687a16FA41bD6055f47c9eBb0);
- 
     uint256 public listingPrice = 0.025 ether;
     mapping(uint256 => ERC20Token) private erc20Tokens;
     mapping(uint256 => Donation) private donations;
     uint256 public _tokenIndex;
     uint256 public _donationIndex;
 
+    bytes32 public s_lastRequestId;
+    bytes public s_lastResponse;
+    bytes public s_lastError;
+
+    error UnexpectedRequestID(bytes32 requestId);
+
     event DonationReceived(address indexed donor, uint256 amount);
     event ERC20TokenListed(uint256 indexed id, address indexed owner, string indexed token);
-    
+    event Response(bytes32 indexed requestId, string result, bytes response, bytes err);
+
+    string source = 
+        "const openaiApiKey = args[0];"
+        "const prompt = args[1];"
+        "const axios = require('axios');"
+        "const response = await axios.post('https://api.openai.com/v1/chat/completions', {"
+        "    model: 'gpt-3.5-turbo',"
+        "    messages: ["
+        "        { role: 'system', content: 'You are a poetic assistant, skilled in explaining complex programming concepts with creative flair.' },"
+        "        { role: 'user', content: prompt }"
+        "    ]"
+        "}, { headers: { 'Authorization': `Bearer ${openaiApiKey}`, 'Content-Type': 'application/json' }});"
+        "if (response.data.error) {"
+        "    throw Error('Request failed');"
+        "}"
+        "return Functions.encodeString(response.data.choices[0].message.content);";
+
+    uint32 gasLimit = 300000;
+
+    bytes32 donID = 0x66756e2d657468657265756d2d7365706f6c69612d3100000000000000000000;
+
+    AggregatorV3Interface internal priceFeed;
 
     modifier onlyOwner() {
         require(
@@ -39,6 +71,10 @@ contract LookUpContract {
             "only owner of the contract can change the listing price"
         );
         _;
+    }
+
+    constructor() FunctionsClient(0xA9d587a00A31A52Ed70D6026794a8FC5E2F5dCb0) ConfirmedOwner(msg.sender) {
+        priceFeed = AggregatorV3Interface(0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419); // ETH/USD price feed address on Ethereum mainnet
     }
 
     function createERC20Token(address _owner, string memory _tokenSupply, string memory _tokenName, string memory _tokenSymbol,
@@ -59,9 +95,8 @@ contract LookUpContract {
         
         emit ERC20TokenListed(_tokenId, _owner, _tokenAddress);
 
-        return ( _tokenId, _owner, _tokenAddress, _tokenName,_tokenSymbol, _tokenAddress);
+        return ( _tokenId, _owner, _tokenAddress, _tokenName, _tokenSymbol, _tokenAddress);
     }
-
 
     function getAllERC20TokenListed() public view returns (ERC20Token[] memory) {
         uint256 itemCount = _tokenIndex;
@@ -120,8 +155,8 @@ contract LookUpContract {
         }
         return items;
     }
-    
-      function getERC20TokenListingPrice() public view returns (uint256) {
+
+    function getERC20TokenListingPrice() public view returns (uint256) {
         return listingPrice;
     }
   
@@ -148,7 +183,6 @@ contract LookUpContract {
             return balance;
     }
   
-
     function donate() external payable {
         require(msg.value > 0, "Donation amount must be greater than 0");
 
@@ -160,7 +194,6 @@ contract LookUpContract {
          donation.donor = msg.sender;
          donation.fund = msg.value;
        
-        
         emit DonationReceived(msg.sender, msg.value);
     }
 
@@ -177,11 +210,39 @@ contract LookUpContract {
         }
         return items;
     }
+
+    function sendRequest(
+        uint64 subscriptionId,
+        string[] calldata args
+    ) external onlyOwner returns (bytes32 requestId) {
+        FunctionsRequest.Request memory req;
+        req.initializeRequestForInlineJavaScript(source); // Initialize the request with JS code
+        if (args.length > 0) req.setArgs(args); // Set the arguments for the request
+
+        // Send the request and store the request ID
+        s_lastRequestId = _sendRequest(
+            req.encodeCBOR(),
+            subscriptionId,
+            gasLimit,
+            donID
+        );
+
+        return s_lastRequestId;
+    }
+
+    function fulfillRequest(
+        bytes32 requestId,
+        bytes memory response,
+        bytes memory err
+    ) internal override {
+        if (s_lastRequestId != requestId) {
+            revert UnexpectedRequestID(requestId); // Check if request IDs match
+        }
+        // Update the contract's state variables with the response and any errors
+        s_lastResponse = response;
+        s_lastError = err;
+
+        // Emit an event to log the response
+        emit Response(requestId, string(response), s_lastResponse, s_lastError);
+    }
 }
-
-
-
-
-
-
-
